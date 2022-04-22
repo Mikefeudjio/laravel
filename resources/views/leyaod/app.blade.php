16e168adca74a0c6b829d9ad139fa8cf468dd6f5
mikefeudjio <mutoyen.feudjio@gmail.com>
2022-04-22 12:00:38 +0100

    create dossier app.scss

--- resources/views/leyaod/app.blade.php
+++ resources/views/leyaod/app.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>mon sper site</title>
+ <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     @include('partials.navbar')
     @yield('content')
+
+   <script src=" {{ asset('js/app.js') }}"></script>
 </body>
 </html>
