d040efa6f2bca5517600f6c5fa4aada5c0819c66
mikefeudjio <mutoyen.feudjio@gmail.com>
2022-04-25 10:37:41 +0100

    creaty tailwind

--- database/migrations/2022_04_25_084947_create_posts_table.php
+++ database/migrations/2022_04_25_084947_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->mediumText('content');
+            $table->$table->mediumText('content');
             $table->timestamps();
         });
     }
